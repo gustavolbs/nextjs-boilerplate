23bb15bf39ab8c121cc61caaf4e9dc71648d1b75
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-21 14:26:43 -0300

    add miragejs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { makeServer } from "../miragejs/server";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+if (process.env.NODE_ENV === "development") {
+  // Mirage JS code will ever reach your production build.
+  makeServer({ environment: "development" });
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
